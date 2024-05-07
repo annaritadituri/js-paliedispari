fae8d7641301b0d8e3bb1f3133ff0cefcdf5bc0c
annaritadituri <annaritadituri@icloud.com>
2024-05-07 23:38:45 +0200

    Solution es pari e dispari

--- paridispari/README.md
+++ paridispari/README.md
@@ -8,3 +8,50 @@ Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 -->
 
+<!--
+FUNZIONE genNumeroRandom()
+-->
+-Prende in input due valori: min, max
+--Genera un numero random in questo range
+---Return : risultato
+
+<!--
+FUNZIONE pariDispari()
+-->
+-Prende in input un valore: totale
+--SE il totale % 2 === 0
+    -Variabile valore = "pari"
+-- ELSE se il totale % 2 !== 0
+    -Variabile valore = "dispari"
+---Return : valore
+
+<!--
+ACQUISIRE IN INPUT
+-->
+-Mettere un ciclo che chieda l'input dall'utente (pari o dispari) fino a che non scrive una delle due alternative corrette
+--Mettere un ciclo che chieda l'input dall'utente (numero) fino a che non scrive un numero da 1 a 5
+
+<!--
+RICHIAMARE FUNZIONE genNumeroRandom()
+-->
+-Creare la variabile numeroComputer = genNumeroRandom(1, 5)
+
+<!--
+CALCOLARE LA SOMMA DI DUE NUMERI
+-->
+-Creare la variabile somma = numeroComputer + numeroUtente
+
+<!--
+RICHIAMARE FUNZIONE pariDispari()
+-->
+-Creare la variabile risultato = pariDispari(somma)
+
+<!--
+CAPIRE IL VINCITORE
+-->
+-SE l'utente aveva scelto "pari" e risultato = "pari"
+    -Stampare il messaggio di vittoria per l'utente
+-ELSE SE l'utente aveva scelto "dispari" e risultato = "dispari"
+    -Stampare il messaggio di vittoria per l'utente
+-ELSE
+    -Stampare il messaggio di vittoria per il computer
--- paridispari/js/main.js
+++ paridispari/js/main.js
@@ -1,3 +1,82 @@
 'use strict';
 
 //////////////////////////////////////////////////////////
+
+//ACQUISIRE IN INPUT
+
+let inputParola;
+
+do {
+
+    inputParola = prompt("Scegli pari o dispari");
+    console.log({inputParola});
+    inputParola = inputParola.toLowerCase();
+    console.log({inputParola});
+
+} while (inputParola !== "pari" && inputParola !== "dispari");
+
+let inputNumero;
+
+do {
+
+    inputNumero = prompt ("Scegli un numero tra 1 e 5 che vuoi giocare:");
+    console.log({inputNumero});
+    inputNumero = parseInt(inputNumero);
+    console.log({inputNumero});
+
+} while (inputNumero > 5 || inputNumero < 1);
+
+//RICHIAMARE FUNZIONE genNumeroRandom()
+let numeroComputer = genNumeroRandom(1, 5);
+console.log({numeroComputer});
+
+//CALCOLARE LA SOMMA DI DUE NUMERI
+let somma = numeroComputer + inputNumero;
+console.log({somma});
+
+//RICHIAMARE FUNZIONE pariDispari()
+let risultato = pariDispari(somma);
+console.log({risultato});
+
+//CAPIRE IL VINCITORE
+if (inputParola === "pari" && risultato === "pari") {
+    console.log("hai vinto con pari");
+} else if (inputParola === "dispari" && risultato === "dispari") {
+    console.log("hai vinto con dispari");
+} else {
+    console.log("hai perso");
+}
+
+///////////////////////////////////////////////////////////
+
+/*
+FUNZIONE genNumeroRandom()
+*/
+function genNumeroRandom(min, max) {
+
+    let numeroRandom = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return numeroRandom;
+
+}
+
+/*
+FUNZIONE pariDispari()
+*/
+function pariDispari(totale) {
+
+    let valore;
+
+    if (totale % 2 === 0) {
+
+        valore = "pari";
+
+    } else {
+
+        valore = "dispari";
+
+    }
+
+    return valore;
+
+}
